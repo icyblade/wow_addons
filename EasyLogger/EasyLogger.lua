ffaf02b8b1d088d48320369d280cc86079071072
Icyblade Dai <icyblade.aspx@gmail.com>
2016-01-31 10:40:19 +0800

    [2.1.0] - 2016-01-31 新增 EasyLogger IcyNameplates 不会那么卡了（大概

---
+++ EasyLogger/EasyLogger.lua
@@ -0,0 +1,27 @@
+local f = CreateFrame("Frame")
+
+f:SetScript("OnEvent", function ()
+	local _, instanceType = IsInInstance()
+	if instanceType == "raid" then
+		if not LoggingCombat() then
+			LoggingCombat(true)
+			print("EasyLogger: |cff00FF00ON|r")
+		end
+	else
+		if LoggingCombat() then
+			LoggingCombat(false)
+			print("EasyLogger: |cffFF0000OFF|r")
+		end
+	end
+end)
+
+SLASH_EASYLOGGER1, EASYLOGGER2 = "/easylogger", "/el"
+SlashCmdList.EASYLOGGER = function()
+	if LoggingCombat() then
+		print("EasyLogger is |cff00FF00ON|r")
+	else
+		print("EasyLogger is |cffFF0000OFF|r")
+	end
+end
+
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
--- IcyNameplates/IcyNameplates.lua
+++ IcyNameplates/IcyNameplates.lua
@@ -1,6 +1,5 @@
 IcyNameplates = LibStub("AceAddon-3.0"):NewAddon("IcyNameplates", "AceEvent-3.0", "AceHook-3.0")
 
-local isInCombat = false
 local nameplates = {}
 
 -- Nameplate
@@ -18,40 +17,23 @@ function Nameplate.create(f)
 end
 
 function Nameplate:update()
+    self.np:Hide()
     self.np:SetHeight(51)
     self.npuf:SetHeight(10)
     self.npufhb:SetHeight(10)
-    -- self.npufhb:SetWidth(100)
+    self.np:Show()
 end
 
-function IcyNameplates:OnEnable() -- OnLoad
+function IcyNameplates:OnEnable()
 	self:RegisterEvent("NAME_PLATE_CREATED", create);
 	self:RegisterEvent("NAME_PLATE_UNIT_ADDED", reset);
-	-- self:RegisterEvent("NAME_PLATE_UNIT_REMOVED", reset);
 	self:RegisterEvent("PLAYER_TARGET_CHANGED", reset);
-	-- self:RegisterEvent("DISPLAY_SIZE_CHANGED", reset);
-	-- self:RegisterEvent("UNIT_AURA", reset);
-	-- self:RegisterEvent("VARIABLES_LOADED", reset);
-	-- self:RegisterEvent("CVAR_UPDATE", reset);
-    -- self:Hook(GameTooltip, "SetUnit", true)
 end
 
 function create(event, f, ...)
     nameplate = Nameplate.create(f)
     table.insert(nameplates, nameplate)
     nameplate:update()
-end
-
-function add(event, f, ...)
-    start_pos, end_pos = strfind(f, '%d')
-    no = string.sub(f, start_pos, end_pos+1)
-    f = _G['NamePlate'..no]
-    name = f:GetName()
-    print('Add '..name)
-    f:SetHeight(51)
-    _G[name..'UnitFrame']:SetHeight(10)
-    _G[name..'UnitFrameHealthBar']:SetHeight(10)
-    _G[name..'UnitFrameHealthBar']:SetWidth(100)
 end
 
 function reset()
